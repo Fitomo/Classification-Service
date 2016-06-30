d1d3b13e7ce6b7ab562dd068c07d747b2af392d5
chris.ng93 <chris.ng93@gmail.com>
2016-06-29 17:46:50 -0700

    complete tuning algorithm

--- algorithm/predicted_health_algorithm.py
+++ algorithm/predicted_health_algorithm.py
@@ -6,6 +6,7 @@ from sklearn import cross_validation
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import mean_squared_error
+from sklearn.externals import joblib
 import matplotlib.pyplot as plt
 
 ###############################################################################
@@ -68,11 +69,9 @@ def gbm_fit(params, cv_folds):
     # check accuracy of model
     # no need for validation data because of cross validation
     # training data is split up into cv_folds folds - model trained on cv_folds - 1 of the folds - last fold is validation set
-    # cv_scores_me = cross_validation.cross_val_score(gbm, x_train, y_train, cv=cv_folds, scoring='mean_absolute_error')
     cv_scores_mse = cross_validation.cross_val_score(gbm, x_train, y_train, cv=cv_folds, scoring='mean_squared_error')
     print '\nModel Report'
-    print ('Mean Squared Error Accuracy: %0.4g (+/- %0.4g)' % (cv_scores_mse.mean(), cv_scores_mse.std()*2))
-    print ('CV Score: Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g' % (np.mean(cv_scores_mse), np.std(cv_scores_mse), np.min(cv_scores_mse), np.max(cv_scores_mse)))
+    print ('MSE Score: Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g' % (np.mean(cv_scores_mse), np.std(cv_scores_mse), np.min(cv_scores_mse), np.max(cv_scores_mse)))
     feat_imp = pd.Series(gbm.feature_importances_, features).sort_values(ascending=False)
     feat_imp.plot(kind='bar', title='Feature Importances')
     plt.ylabel('Feature Importance Score')
@@ -83,45 +82,52 @@ def gbm_fit(params, cv_folds):
     test['health_score_in_week'] = final_predictions
     test.to_csv(output_file, columns=['user_id', 'date', 'steps', 'total_sleep', 'resting_hr', 'step_week_slope', 'sleep_week_slope', 'hr_week_slope', 'curr_health_score', 'health_score_in_week'])
 
-# test various parameters
+    # save the model to disk
+    joblib.dump(gbm, 'health_prediction.pkl', compress=1)
+
 params_original = {
-'min_samples_split': 500,
-'min_samples_leaf': 50,
-'max_depth': 8,
-'max_features': 'sqrt',
-'subsample': 0.8,
-'learning_rate': 0.1,
-'n_estimators': 500,
-'random_state': 10,
-'loss': 'ls'
+    'min_samples_split': 500,
+    'min_samples_leaf': 50,
+    'max_depth': 8,
+    'max_features': 'sqrt',
+    'subsample': 0.8,
+    'learning_rate': 0.1,
+    'n_estimators': 500,
+    'random_state': 10,
+    'loss': 'ls'
 }
 # gbm_fit(params_original, 5)
 
+params_tuned = {
+    'min_samples_split': 2000,
+    'min_samples_leaf': 30,
+    'max_depth': 7,
+    'max_features': 2,
+    'subsample': 0.8,
+    'learning_rate': 0.005,
+    'n_estimators': 1500,
+    'random_state': 10,
+    'loss': 'ls'
+}
+gbm_fit(params_tuned, 5)
+
 ###############################################################################
 # tune parameters
-param_test1 = { 'n_estimators':range(20,121,10) }
+param_test1 = { 'n_estimators':range(20,81,10) }
 grid_search1 = GridSearchCV(
     estimator=GradientBoostingRegressor(learning_rate=0.05,min_samples_split=500,min_samples_leaf=50,max_depth=8,max_features='sqrt',subsample=0.8,random_state=10),
     param_grid=param_test1,
     scoring='mean_squared_error',
     n_jobs=4,
     iid=False,
-    cv=2
+    cv=3
 )
-grid_search1.fit(x_train, y_train)
-grid_search1.grid_scores_, grid_search1.best_params_, grid_search1.best_score_
-# [ mean: -144.50046, std: 1.56585, params: {'n_estimators': 20}, mean: -137.16978, std: 1.26331, params: {'n_estimators': 30},
-#   mean: -134.14077, std: 1.12795, params: {'n_estimators': 40}, mean: -132.88186, std: 1.03599, params: {'n_estimators': 50},
-#   mean: -132.36311, std: 0.97073, params: {'n_estimators': 60}, mean: -132.14041, std: 0.91713, params: {'n_estimators': 70},
-#   mean: -132.05814, std: 0.89192, params: {'n_estimators': 80}, mean: -132.01237, std: 0.87023, params: {'n_estimators': 90},
-#   mean: -132.00162, std: 0.85715, params: {'n_estimators': 100}, mean: -132.00080, std: 0.84445, params: {'n_estimators': 110},
-#   mean: -132.00731, std: 0.84576, params: {'n_estimators': 120} ]
-# {'n_estimators': 110}
-# -132.000795686
+# grid_search1.fit(x_train, y_train)
+# print grid_search1.grid_scores_, grid_search1.best_params_, grid_search1.best_score_
 
 # tune max_depth and min_samples_split because they're higher impact
-param_test2 = { 'max_depth':range(5,16,2), 'min_samples_split':range(200,1001,200) }
-grid_search1 = GridSearchCV(
+param_test2 = { 'max_depth':range(5,16,2), 'min_samples_split':range(1600,2400,200) }
+grid_search2 = GridSearchCV(
     estimator=GradientBoostingRegressor(learning_rate=0.05,n_estimators=110,min_samples_leaf=50,max_features='sqrt',subsample=0.8,random_state=10),
     param_grid=param_test2,
     scoring='mean_squared_error',
@@ -129,5 +135,44 @@ grid_search1 = GridSearchCV(
     iid=False,
     cv=3
 )
-grid_search1.fit(x_train, y_train)
-grid_search1.grid_scores_, grid_search1.best_params_, grid_search1.best_score_
+# grid_search2.fit(x_train, y_train)
+# print grid_search2.grid_scores_, grid_search2.best_params_, grid_search2.best_score_
+
+# tune min_samples_leaf
+param_test3 = { 'min_samples_leaf':range(20,61,10) }
+grid_search3 = GridSearchCV(
+    estimator=GradientBoostingRegressor(learning_rate=0.05,n_estimators=110,min_samples_split=2000,max_depth=7,max_features='sqrt',subsample=0.8,random_state=10),
+    param_grid=param_test3,
+    scoring='mean_squared_error',
+    n_jobs=4,
+    iid=False,
+    cv=3
+)
+# grid_search3.fit(x_train, y_train)
+# print grid_search3.grid_scores_, grid_search3.best_params_, grid_search3.best_score_
+
+# tune max_features
+param_test4 = { 'max_features':range(1,6,1) }
+grid_search4 = GridSearchCV(
+    estimator=GradientBoostingRegressor(learning_rate=0.05,n_estimators=110,min_samples_split=2000,max_depth=7,min_samples_leaf=30,subsample=0.8,random_state=10),
+    param_grid=param_test4,
+    scoring='mean_squared_error',
+    n_jobs=4,
+    iid=False,
+    cv=3
+)
+# grid_search4.fit(x_train, y_train)
+# print grid_search4.grid_scores_, grid_search4.best_params_, grid_search4.best_score_
+
+# tune subsample
+param_test5 = { 'subsample':[0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9] }
+grid_search5 = GridSearchCV(
+    estimator=GradientBoostingRegressor(learning_rate=0.05,n_estimators=110,min_samples_split=2000,max_depth=7,min_samples_leaf=30,max_features=2,random_state=10),
+    param_grid=param_test5,
+    scoring='mean_squared_error',
+    n_jobs=4,
+    iid=False,
+    cv=3
+)
+# grid_search5.fit(x_train, y_train)
+# print grid_search5.grid_scores_, grid_search5.best_params_, grid_search5.best_score_
